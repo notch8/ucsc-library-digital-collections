177dca18f01f572cda21b954fa675d83dc520228
Ryan Schwab <rwschwab@ucsc.edu>
2021-02-09 14:14:06 -0800

    Collections A-Z page initial commit.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,7 +1,7 @@
 class CollectionsController < Hyrax::CollectionsController
   layout :resolve_layout
   self.presenter_class = Ucsc::CollectionPresenter
-  load_and_authorize_resource except: [:index, :create], instance_name: :collection
+  load_and_authorize_resource except: [:index, :create, :show_all], instance_name: :collection
 
   def resolve_layout
     query_collection_members
@@ -11,6 +11,9 @@ class CollectionsController < Hyrax::CollectionsController
 
   def search_action_url options = {}
     url_for(options.reverse_merge(action: 'index', controller: 'catalog').deep_merge(f: {"ancestor_collection_titles_ssim" => Array(@collection.title)}))
+  end
+
+  def show_all
   end
   
 end
