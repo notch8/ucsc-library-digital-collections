0434efd40e28038aea621f17c47a055e1ded5fad
Ryan Schwab <rwschwab@ucsc.edu>
2021-02-22 14:47:47 -0800

    A-Z Collections page progress, move strings into locale config file.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,9 +2,10 @@ class CollectionsController < Hyrax::CollectionsController
   layout :resolve_layout
   self.presenter_class = Ucsc::CollectionPresenter
   skip_load_and_authorize_resource :only => :show_all
+  load_and_authorize_resource except: [:index, :create], instance_name: :collection
 
   def resolve_layout
-    return "hyrax" if action_name == "show_all"
+    return "hyrax/1_column" if action_name == "show_all"
     query_collection_members
     return "collection_with_subcollections" if @subcollection_count > 0
     "hyrax"
@@ -15,6 +16,12 @@ class CollectionsController < Hyrax::CollectionsController
   end
 
   def show_all
+	#@collections = collections
+	builder = Hyrax::CollectionSearchBuilder.new(self)
+    @response = repository.search(builder)
+    @collections = @response.documents.select{ |col| ["open","campus"].include?(col.visibility)}
+  rescue Blacklight::Exceptions::ECONNREFUSED, Blacklight::Exceptions::InvalidRequest
+    []
   end
-  
+
 end
