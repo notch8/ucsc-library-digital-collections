21b46051a7e76e6d2e35c3958f53c4db2e9f78fb
Benjamin Kiah Stroud <kiah@notch8.com>
2021-11-23 15:38:17 -0800

    functional authorities by URI

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -3,6 +3,11 @@
 module Bulkrax::HasLocalProcessing
   include ControlledIndexerBehavior
 
+  SOURCES_OF_AUTHORITIES = [
+    ::Qa::Authorities::Local, # local db, faster
+    ::Qa::Authorities::Loc # remote, slower
+  ].freeze
+
   # This method is called during build_metadata
   # add any special processing here, for example to reset a metadata property
   # to add a custom property from outside of the import data
@@ -20,17 +25,23 @@ module Bulkrax::HasLocalProcessing
       parsed_metadata.delete(field_name) # remove non-standardized values
       next if raw_metadata[field_name].blank?
 
+      # TODO: ensure existing values don't get lost
       raw_metadata[field_name].split(/\s*[|]\s*/).uniq.each_with_index do |value, i|
-        data = if value.match?(::URI::DEFAULT_PARSER.make_regexp)
-                 { id: value }
-               else
-                 authority = ::Qa::Authorities::Loc.subauthority_for('names') # TODO: might need to support more subauthorities
-                 found_id = authority.search(value)&.first&.dig('id')
-
-                 found_id.present? ? { id: found_id } : nil
-               end
-        # TODO: permit in obj factory
-        parsed_metadata["#{field_name}_attributes"][i] = data
+        auth_id = if value.match?(::URI::DEFAULT_PARSER.make_regexp)
+                    value # assume raw, user-provided URI is a valid authority
+                  else # attempt to lookup id by label
+                    found_id = nil
+                    SOURCES_OF_AUTHORITIES.each do |auth_source|
+                      auth_source.subauthorities.each do |subauth|
+                        subauthority = auth_source.subauthority_for(subauth)
+                        found_id = subauthority.search(value)&.first&.dig('id')
+                        break if found_id.present?
+                      end
+                    end
+                    found_id # mintLocalAuthUrl if nil
+                  end
+        parsed_metadata["#{field_name}_attributes"] ||= {}
+        parsed_metadata["#{field_name}_attributes"][i] = { 'id' => auth_id } if auth_id.present?
       end
     end
   end
