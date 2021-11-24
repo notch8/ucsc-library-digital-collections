829cb5e8998382f85952ea43f7e47c3f097edad5
Benjamin Kiah Stroud <kiah@notch8.com>
2021-11-24 12:02:52 -0800

    split logic into descriptive methods, add documentation, cleanup

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -23,36 +23,43 @@ module Bulkrax::HasLocalProcessing
     add_controlled_fields
   end
 
+  # Controlled fields expect an ActiveTriples instance as a value. Bulkrax only imports strings.
+  # Use the imported string values to lookup or create valid ActiveTriples URIs and add them
+  # to the Entry's parsed_metadata in the format that the actor stack expects.
   def add_controlled_fields
     metadata_schema = ::ScoobySnacks::METADATA_SCHEMA
 
     metadata_schema.controlled_field_names.each do |field_name|
+      field = metadata_schema.get_field(field_name)
       parsed_metadata.delete(field_name) # remove non-standardized values
       next if raw_metadata[field_name.downcase].blank?
 
-      # TODO: ensure existing values don't get lost
       raw_metadata[field_name.downcase].split(/\s*[|]\s*/).uniq.each_with_index do |value, i|
-        auth_id = if value.match?(::URI::DEFAULT_PARSER.make_regexp)
-                    value # assume raw, user-provided URI is a valid authority
-                  else # find or create an authority
-                    found_id = nil
-                    field = metadata_schema.get_field(field_name)
-                    SOURCES_OF_AUTHORITIES.each do |auth_source, auth_name| # attempt to lookup id
-                      subauth_name = get_subauthority_for(field: field, authority_name: auth_name)
-                      next unless subauth_name.present?
+        auth_id = value if value.match?(::URI::DEFAULT_PARSER.make_regexp) # assume raw, user-provided URI is a valid authority
+        auth_id ||= search_authorities_for_id(field, value)
+        auth_id ||= create_local_authority_id(field, value)
+        next unless auth_id.present?
 
-                      subauthority = auth_source.subauthority_for(subauth_name)
-                      found_id = subauthority.search(value)&.first&.dig('id')
-                    end
-                    if found_id.blank? # create local auth if unable to find one
-                      local_subauth_name = get_subauthority_for(field: field, authority_name: 'local')
-                      found_id = mint_local_auth_url(local_subauth_name, value) if local_subauth_name.present?
-                    end
-                    found_id
-                  end
         parsed_metadata["#{field_name}_attributes"] ||= {}
-        parsed_metadata["#{field_name}_attributes"][i] = { 'id' => auth_id } if auth_id.present?
+        parsed_metadata["#{field_name}_attributes"][i] = { 'id' => auth_id }
       end
     end
+  end
+
+  # @return [String, nil] URI for authority, or nil if one could not be found
+  def search_authorities_for_id(field, value)
+    SOURCES_OF_AUTHORITIES.each do |auth_source, auth_name|
+      subauth_name = get_subauthority_for(field: field, authority_name: auth_name)
+      next unless subauth_name.present?
+
+      subauthority = auth_source.subauthority_for(subauth_name)
+      subauthority.search(value)&.first&.dig('id')
+    end
+  end
+
+  # @return [String, nil] URI for local authority, or nil if one could not be created
+  def create_local_authority_id(field, value)
+    local_subauth_name = get_subauthority_for(field: field, authority_name: 'local')
+    mint_local_auth_url(local_subauth_name, value) if local_subauth_name.present?
   end
 end
